7bc20677be82cf50fd8fff24f778aed075ab860e
Taro Morita <morita0079@gmail.com>
2024-02-02 11:59:36 +0900

    chore(root.tsx): remove commented out code for better code readability and maintainability

--- app/root.tsx
+++ app/root.tsx
@@ -48,11 +48,9 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                {/* <a href={`/contacts/1`}>Your Name</a> */}
                 <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                {/* <a href={`/contacts/2`}>Your Friend</a> */}
                 <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
